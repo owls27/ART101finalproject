5680471cbc81f4ace9d977b333f4d7e445959008
owls27 <tzuhyo0927@gmail.com>
2021-12-09 05:05:29 -0800

    updated

--- sketch.js
+++ sketch.js
@@ -5,12 +5,14 @@ let cnv;
 let points = 0;
 let w = 600;
 let h = 600;
+let player;
+let coin;
 
 function setup() {
   cnv = createCanvas(w, h);
-
   textFont('monospace');
-
+  player = new Player();
+  coin = new Coin();
 }
 
 function draw() {
@@ -24,7 +26,7 @@ function draw() {
       level1();
       cnv.mouseClicked(level1MouseClicked);
       break;
-      case 'you win':
+    case 'you win':
       youWin();
       cnv.mouseClicked(youWinMouseClicked);
       break;
@@ -49,15 +51,29 @@ function draw() {
 //   state = 'level 1';
 // }
 
+function keyPressed() {
+  if (keyCode == LEFT_ARROW) {
+    player.direction = 'left'
+  } else if (keyCode == RIGHT_ARROW) {
+    player.direction = 'right'
+  } else if (keyCode == UP_ARROW) {
+    player.direction = 'up'
+  } else if (keyCode == DOWN_ARROW) {
+    player.direction = 'down'
+  } else if (key = ' ') {
+    player.direction = 'still'
+  }
+}
+
 function title() {
   background(0);
   textSize(80);
   fill(255);
   textAlign(CENTER);
-  text('MY GAME', w/2, h/5);
+  text('MY GAME', w / 2, h / 5);
 
   textSize(30);
-  text('click anywhere to start', w/2, h/2);
+  text('click anywhere to start', w / 2, h / 2);
 }
 
 function titleMouseClicked() {
@@ -67,28 +83,33 @@ function titleMouseClicked() {
 
 function level1() {
   background(50, 150, 200);
-  text('click for points', w/2, h - 30);
+  //text('click for points', w/2, h - 30);
+
+  player.display();
+  player.move();
+  coin.display();
+  coin.move();
 }
 
 function level1MouseClicked() {
   points++;
   console.log('points = ' + points);
-  if(points >= 10){
+  if (points >= 10) {
     state = 'you win';
   }
 }
 
-function youWin(){
+function youWin() {
   background(255, 50, 80);
   textSize(80);
   stroke(255);
-  text('YOU WIN', w/2, h/2);
+  text('YOU WIN', w / 2, h / 2);
 
   textSize(30);
-  text('click anywhere to restart', w/2, h * 3/4);
+  text('click anywhere to restart', w / 2, h * 3 / 4);
 }
 
-function youWinMouseClicked(){
+function youWinMouseClicked() {
   state = 'level 1';
   points = 0;
 }
